726c383db092c1f5bc0c7dcc1e8f93614100cd55
jakedevar <devarennesjacob@gmail.com>
2023-06-13 07:55:56 -0700

    updated to use actix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App,HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn health_check(req: HttpRequest) -> Responder {
+    HttpResponse::Ok().finish()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
