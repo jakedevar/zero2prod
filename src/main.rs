e523ac924d642f8b3a4f27dff2d348d409f8e5f3
jakedevar <devarennesjacob@gmail.com>
2023-06-13 08:24:44 -0700

    implemented health check

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 use actix_web::{get, web, App,HttpRequest, HttpResponse, HttpServer, Responder};
 
-async fn health_check(req: HttpRequest) -> Responder {
-    HttpResponse::Ok().finish()
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
 }
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
     })
     .bind("127.0.0.1:8000")?
     .run()
