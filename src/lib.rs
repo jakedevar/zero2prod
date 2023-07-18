1fa2041556d578e6eb2d3a35773e4910bd269460
jakedevar <devarennesjacob@gmail.com>
2023-07-17 17:47:25 -0700

    stopping p. 66

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,26 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    username: String,
+}
+
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+async fn subscribe(form: web::Form<FormData>) -> String {
+    format!("Welcome {}!", form.username)
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
         .listen(listener)?
         .run();
 
